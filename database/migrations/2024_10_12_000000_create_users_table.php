737167618537a44722ba21213244db324a076c2a
Bernardinus Hendra Natadiria <me@hendranata.com>
2020-11-26 00:55:10 +0700

    User table update

--- database/migrations/2024_10_12_000000_create_users_table.php
+++ database/migrations/2024_10_12_000000_create_users_table.php
@@ -19,8 +19,11 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->unsignedBigInteger('cleaning_services_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
+
+            $table->foreign('cleaning_services_id')->references('id')->on('cleaning_services');
         });
     }
 
